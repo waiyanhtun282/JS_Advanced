74d2fd12e11fc50ed8550b7916058dd5217adf74
waiyan282 <waiyanhtun282@gmail.com>
2023-12-26 19:53:26 +0630

    finishpassbyreference

---
+++ passByVlaueandReference/index.js
@@ -0,0 +1,49 @@
+// let a = 5;
+// let b = a;
+// b++;
+
+// console.log(a);
+// console.log(b);
+// // pass by value and reference
+// let obj1 = { name: "Leo", password: "1232" };
+// let obj2 = obj1;
+// obj2.password = "45676";
+
+// console.log(obj1);
+
+// let d = [1, 2, 3, 4, 5];
+// let e = d;
+// e.push("12323");
+// console.log(e);
+// console.log(d);
+
+// let obj = {
+//   a: "b",
+//   b: "c",
+//   c: {
+//     deep: " try and copy code",
+//   },
+// };
+
+// let clone = Object.assign({}, obj);
+// let clone2 = { ...obj };
+// let superClone = JSON.parse(JSON.stringify(obj));
+// obj.c.d = "hahahaha";
+// console.log(obj);
+// console.log(clone);
+// console.log(clone2);
+// console.log(superClone);
+
+// Exercise Comapre Objects
+
+let user1 = { name: "nerd", org: "dev" };
+let user2 = { name: "nerd", org: "dev" };
+// give code
+// let eq = user1 == user2;
+
+// fix code
+let eq = JSON.stringify(user1) == JSON.stringify(user2);
+
+console.log(eq);
+
+// pass by referece
