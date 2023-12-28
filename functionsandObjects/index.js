9637f7c4c56147f58713096c7d9ac7cff72a620f
waiyan282 <waiyanhtun282@gmail.com>
2023-12-28 19:40:21 +0630

    finishfunctinisObjects

---
+++ functionsandObjects/index.js
@@ -0,0 +1,36 @@
+// functions is objects
+
+// const obj ={
+//     two:function(){
+//         return 2;
+//     }
+// };
+
+
+//  two will be method that is ()
+// const obj ={
+//     two(){ 
+//         return 2;
+//     }
+// };
+// console.log(obj.two());
+
+// const four = new   Function('retrun 4');
+// console.log(four());
+
+
+// code() name 
+// property and method=>call(), apply(),bind
+// function whoooo(){
+//     console.log('whoooo');
+// };
+// whoooo.name
+// whoooo.bind
+// whoooo.yell ="heeeeee";
+
+// const specialObj ={
+//     name:'whoooo',
+//     yell:'ahhhhh',
+//     ():console.log('whoooo')
+
+// }
