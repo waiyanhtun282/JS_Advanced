1fd2993dd88c01512b948804c5da373df9b7eb3f
waiyan282 <waiyanhtun282@gmail.com>
2024-01-01 19:57:56 +0630

    devGarymiddleShallowcopy

--- passByVlaueandReference/index.js
+++ passByVlaueandReference/index.js
@@ -65,3 +65,71 @@
 // Structural
 // 1 Objects: new Objects,Array,Map,Set,WeakMap,Data
 // 2 Functions
+
+// Value vs Reference
+// Primitive Reference
+let x = 2;
+let y = x;
+y += 1;
+
+console.log(x);
+console.log(y);
+
+// strucura type reference
+let xArray = [1, 2, 3, 4, 5];
+let yArray = xArray;
+yArray.push(6);
+console.log(xArray);
+console.log(yArray);
+
+// mutabel and imutable
+
+// primitive are immutalbel
+let myName = "Dave";
+myName[0] = "W"; //nope
+console.log(myName);
+
+// Reassign  is not same as mutable
+// myName = "David";
+// console.log(myName);
+// sturctural contain mutal data
+// yArray[0] = 9;
+// console.log(yArray);
+// console.log(xArray);
+
+// Pure Functuion  require you to avoid
+// mutating data
+
+// Imputing function that mutates data
+const addToScoreHistory = (array, score) => {
+  array.push(score);
+  return array;
+};
+
+const scoreArray = [44, 22, 32];
+console.log(addToScoreHistory(scoreArray, 14));
+// This mutate the original array;
+// This is consdiders is side-effect;
+
+// Notice: 'const' doesn't make array immutable
+
+// there is much more to pure function that will dicuss  in my next tutorial
+
+// shallow copy vs deep copy(clones)
+
+// shallow copy
+// with the spred operator
+const zArray = [...yArray, 10];
+console.log(zArray);
+console.log(yArray);
+
+console.log(xArray === yArray);
+console.log(zArray === yArray);
+
+// with Object.assign();
+const tArray = Object.assign([], zArray);
+console.log(tArray);
+console.log(tArray === zArray);
+tArray.push(11);
+console.log(zArray);
+console.log("tArray", tArray);
