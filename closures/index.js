0fe80ce2be7d78f2e41ab47d3dd3c2603e1dcf1e
waiyan282 <waiyanhtun282@gmail.com>
2024-01-01 13:00:36 +0630

    devGrayclosure

--- closures/index.js
+++ closures/index.js
@@ -30,3 +30,28 @@
 //   const callMe = "Hi I am now here!";
 // }
 // callMayBe();
+
+// functions
+// This is often consfused with closure, but lexical scope is only an imporatant
+// part of closure
+
+// w3schools:'A closure is a function having access to the parent scope even after the parent function has closed'
+//A closure is  created when we define a function, not when a function is execucuted
+
+// global scope
+let x = 1;
+const parenfunc = () => {
+  let myValue = 2;
+  console.log(myValue);
+  console.log(x);
+  const childFunv = () => {
+    console.log((x += 5));
+    console.log((myValue += 2));
+  };
+
+  return childFunv;
+};
+const result = parenfunc();
+console.log(result);
+result();
+result();
