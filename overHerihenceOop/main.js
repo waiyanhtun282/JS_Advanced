b043aeb9b1fafd6dcf31695193a011245618db86
waiyanhtun282 <waiyanhtun282@gmail.com>
2024-02-11 23:18:33 +0630

    ADD_FUNCION

--- overHerihenceOop/main.js
+++ overHerihenceOop/main.js
@@ -1,3 +1,38 @@
+const prepare = () =>{
+   return {
+      prepare:() =>console.log('Preparing...')
+   }
+}
+
+const bake = () =>{
+   return {
+      bake:() =>console.log('Baraking...')
+   }
+}
+
+
+const toss = () =>{
+   return {
+      toss:() =>console.log('Baking...')
+   }
+}
+
+const ready = () =>{
+   return {
+      ready:() =>console.log('Ready!...')
+   }
+}
+const stuff = () =>{
+   return {
+      stuff() {console.log('stuff!...')}
+   }
+}
+
+const butter = () =>{
+   return {
+      butter() {console.log('buttering!...')}
+   }
+}
 const createPizza =(size,curst,sauce) =>{
  const pizza ={
     size:size,
