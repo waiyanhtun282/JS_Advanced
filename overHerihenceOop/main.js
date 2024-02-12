924ff90933857c80654c734fcdbefe57fdda5d1a
waiyanhtun282 <waiyanhtun282@gmail.com>
2024-02-12 22:14:26 +0630

    ADD_FUNCTION

--- overHerihenceOop/main.js
+++ overHerihenceOop/main.js
@@ -22,6 +22,7 @@ const ready = () =>{
       ready:() =>console.log('Ready!...')
    }
 }
+
 const stuff = () =>{
    return {
       stuff() {console.log('stuff!...')}
@@ -33,17 +34,69 @@ const butter = () =>{
       butter() {console.log('buttering!...')}
    }
 }
-const createPizza =(size,curst,sauce) =>{
- const pizza ={
-    size:size,
-    curst:curst,
-    sauce:sauce,
-    toppings:[]
- }
+
+
+
+const createPizza = (size,crust,sauce) =>{
+       const pizza = {
+         size:size,
+         crust:crust,
+         sauce:sauce,
+         toppings:[]
+       }
+
+       return {
+         ...pizza,
+         ...prepare(),
+         ...bake(),
+         ...ready()
+       }
+}
+
+
+const createSlade =(size,dressing) =>{
+
  return {
-    ...pizza,
-    ...curst,
-    ...sauce,
+    size:size,
+    dressing:dressing,
+
+   ...prepare(),
+   ...toss(),
+   ...ready()
     
  }
 }
+
+// Compare to ES6 Class syntx with extends and super()
+const creatStuffedBufferedCrustPizza =(pizza) =>{
+   return {
+      ...pizza,
+      ...stuff(),
+      ...butter()
+   }
+}
+
+
+const anotherPizza =createPizza('medium','thin','original');
+const somebodyPizza =creatStuffedBufferedCrustPizza(anotherPizza);
+// OR
+const davPizza =creatStuffedBufferedCrustPizza(createPizza('medium','thin','original'))
+const davesSlade = createSlade('side','branch');
+
+davPizza.bake();
+davPizza.stuff();
+davesSlade.prepare();
+
+console.log(davPizza);
+console.log(davesSlade);
+
+// what about toppings?
+const addTopping =(pizza,topping) =>{
+   pizza.toppings.push(topping);
+   return pizza;
+};
+
+const jimsPizza =createPizza('medium','thin','original')
+console.log(jimsPizza);
+console.log(addTopping(jimsPizza,'pepperoni'));
+console.log('mutation',jimsPizza);//mutation
